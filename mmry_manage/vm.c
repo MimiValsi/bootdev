0f8be6d820fff3bd4fb2680ebbf816b520f26bc6
Miguel Da Silva <miguel@dasilvaf.net>
2024-11-12 20:03:48 +0100

    Memory finished

--- mmry_manage/vm.c
+++ mmry_manage/vm.c
@@ -1,6 +1,63 @@
 #include "vm.h"
 #include "stack.h"
 
+void vm_collect_garbage(vm_t *vm) {
+        // ?
+        mark(vm);
+        trace(vm);
+        sweep(vm);
+}
+
+void sweep(vm_t *vm) {
+        // ?
+        for (int i = 0; i < vm->objects->count; i++) {
+                snek_object_t *obj = vm->objects->data[i];
+                if (obj->is_marked) {
+                        obj->is_marked = false;
+                        continue;
+                }
+                snek_object_free(obj);
+                vm->objects->data[i] = NULL;
+        }
+        stack_remove_nulls(vm->objects);
+}
+
+void trace(vm_t *vm) {
+        // ?
+        stack_t *gray_objects = stack_new(8);
+        if (!gray_objects) {
+                return;
+        }
+        for (size_t i = 0; i < vm->objects->count; i++) {
+                snek_object_t *obj = vm->objects->data[i];
+                if (obj->is_marked) {
+                        stack_push(gray_objects, obj);
+                }
+        }
+        while (gray_objects->count > 0) {
+                trace_blacken_object(gray_objects, stack_pop(gray_objects));
+        }
+        stack_free(gray_objects);
+}
+
+void trace_blacken_object(stack_t *gray_objects, snek_object_t *obj) {
+        // ?
+        if (obj->kind == INTEGER || obj->kind == FLOAT || obj->kind == STRING) {
+                return;
+        }
+        if (obj->kind == VECTOR3) {
+                trace_mark_object(gray_objects, obj->data.v_vector3.x);
+                trace_mark_object(gray_objects, obj->data.v_vector3.y);
+                trace_mark_object(gray_objects, obj->data.v_vector3.z);
+        }
+        if (obj->kind == ARRAY) {
+                for (size_t i = 0; i < obj->data.v_array.size; i++) {
+                        trace_mark_object(gray_objects,
+                                          obj->data.v_array.elements[i]);
+                }
+        }
+}
+
 void mark(vm_t *vm) {
         // ?
         for (size_t i = 0; i < vm->frames->count; i++) {
