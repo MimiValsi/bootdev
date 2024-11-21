cb1473a44588809378e95234aa71fe99eaa667ca
Miguel Da Silva <miguel@dasilvaf.net>
2024-11-21 21:50:06 +0100

    update go

--- go/slices.go
+++ go/slices.go
@@ -17,3 +17,59 @@ func getCostsByDay(costs []cost) []float64 {
 	return costsByDay
 }
 
+// 2 ways of doing it
+func createMatrix(rows, cols int) [][]int {
+	matrix := make([][]int, rows)
+	for i := 0; i < rows; i++ {
+		matrix[i] = make([]int, cols)
+		for j := 0; j < cols; j++ {
+			// matrix[i] = append(matrix[i], i*j)
+			matrix[i][j] = i * j
+		}
+	}
+
+	return matrix
+}
+
+type Message interface {
+	Type() string
+}
+
+type TextMessage struct {
+	Sender  string
+	Content string
+}
+
+func (tm TextMessage) Type() string {
+	return "text"
+}
+
+type MediaMessage struct {
+	Sender    string
+	MediaType string
+	Content   string
+}
+
+func (mm MediaMessage) Type() string {
+	return "media"
+}
+
+type LinkMessage struct {
+	Sender  string
+	URL     string
+	Content string
+}
+
+func (lm LinkMessage) Type() string {
+	return "link"
+}
+
+func filterMessages(messages []Message, filterType string) []Message {
+	var filtered []Message
+	for _, message := range messages {
+		if message.Type() == filterType {
+			filtered = append(filtered, message)
+		}
+	}
+	return filtered
+}
