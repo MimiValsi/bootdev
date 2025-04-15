f418f1f6e2d14986f20555880d5f91dcf438036b
Miguel DA SILVA <miguel@dasilvaf.net>
2025-04-15 14:46:25 +0200

    separate handlers by file.

--- chirpy/main.go
+++ chirpy/main.go
@@ -40,21 +40,3 @@ func main() {
 	log.Fatal(s.ListenAndServe())
 
 }
-
-func handlerReadiness(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(http.StatusText(http.StatusOK)))
-}
-
-func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
-	cfg.fileserverHits.Store(0)
-	w.WriteHeader(http.StatusOK)
-}
-
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits.Add(1)
-		next.ServeHTTP(w, r)
-	})
-}
