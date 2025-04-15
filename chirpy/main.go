574cfe74def919bbc30cc8eda389360d4baf9b8d
Miguel DA SILVA <miguel@dasilvaf.net>
2025-04-15 14:15:40 +0200

    add /api path

--- chirpy/main.go
+++ chirpy/main.go
@@ -25,9 +25,10 @@ func main() {
 
 	handler := http.StripPrefix("/app", http.FileServer(http.Dir(fpRoot)))
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(handler))
-	mux.HandleFunc("GET /healthz", handlerReadiness)
-	mux.HandleFunc("GET /metrics", apiCfg.handlerMetrics)
-	mux.HandleFunc("POST /reset", apiCfg.handlerReset)
+
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.HandleFunc("GET /api/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("POST /api/reset", apiCfg.handlerReset)
 
 	s := &http.Server{
 		Addr:    ":" + port,
