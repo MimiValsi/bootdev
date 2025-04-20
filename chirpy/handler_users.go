35d2fa53802e5e4b78149628ac36190f603187c3
Miguel DA SILVA <miguel@dasilvaf.net>
2025-04-20 20:00:42 +0200

    remove comments

--- chirpy/handler_users.go
+++ chirpy/handler_users.go
@@ -5,19 +5,11 @@ import (
 	"net/http"
 )
 
-// no method allowed...
 func middlewareDB(cfg *apiConfig) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		type parameters struct {
 			Email string `json:"email"`
 		}
-
-		// type returnVals struct {
-		// 	Id        uuid.UUID `json:"id"`
-		// 	CreatedAt time.Time `json:"created_at"`
-		// 	UpdatedAt time.Time `json:"updated_at"`
-		// 	Email     string    `json:"email"`
-		// }
 
 		decoder := json.NewDecoder(r.Body)
 		params := parameters{}
@@ -26,10 +18,6 @@ func middlewareDB(cfg *apiConfig) http.Handler {
 			respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters", err)
 			return
 		}
-
-		// this ain't good!
-		// need to find way to fetch apiConfig pointer...
-		// cfg := apiConfig{}
 
 		user, err := cfg.db.CreateUser(r.Context(), params.Email)
 		if err != nil {
@@ -45,41 +33,4 @@ func middlewareDB(cfg *apiConfig) http.Handler {
 		respondWithJSON(w, http.StatusCreated, retUser)
 
 	})
-}
-func handlerUsers(w http.ResponseWriter, r *http.Request) {
-	type parameters struct {
-		Email string `json:"email"`
-	}
-
-	// type returnVals struct {
-	// 	Id        uuid.UUID `json:"id"`
-	// 	CreatedAt time.Time `json:"created_at"`
-	// 	UpdatedAt time.Time `json:"updated_at"`
-	// 	Email     string    `json:"email"`
-	// }
-
-	decoder := json.NewDecoder(r.Body)
-	params := parameters{}
-	err := decoder.Decode(&params)
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters", err)
-		return
-	}
-
-	// this ain't good!
-	// need to find way to fetch apiConfig pointer...
-	cfg := apiConfig{}
-
-	user, err := cfg.db.CreateUser(r.Context(), params.Email)
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Couldn't create user", err)
-	}
-	retUser := User{
-		ID:        user.ID,
-		CreatedAt: user.CreatedAt,
-		UpdatedAt: user.UpdatedAt,
-		Email:     user.Email,
-	}
-
-	respondWithJSON(w, http.StatusCreated, retUser)
 }
