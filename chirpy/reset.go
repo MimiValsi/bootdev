d12904c5837579cdbc3ce7226a5f871491b1aa4c
Miguel DA SILVA <miguel@dasilvaf.net>
2025-04-20 20:01:08 +0200

    check if platform field is set to "dev"

--- chirpy/reset.go
+++ chirpy/reset.go
@@ -4,5 +4,12 @@ import "net/http"
 
 func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
 	cfg.fileserverHits.Store(0)
+
+	if cfg.platform != "dev" {
+		respondWithError(w, http.StatusForbidden, "Must be dev to drop from users", nil)
+		return
+	}
+
+	cfg.db.DeleteUsers(r.Context())
 	w.WriteHeader(http.StatusOK)
 }
