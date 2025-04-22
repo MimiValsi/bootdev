df9ff38d0f2483f7234bb9f1156b3a084146af78
Miguel DA SILVA <miguel@dasilvaf.net>
2025-04-22 19:46:09 +0200

    bootdev corrections

--- chirpy/handler_users.go
+++ chirpy/handler_users.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"log"
 	"mimivalsi/chirpy/internal/auth"
 	"mimivalsi/chirpy/internal/database"
 	"net/http"
@@ -16,6 +15,7 @@ type User struct {
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 	Email     string    `json:"email"`
+	Password  string    `json:"-"`
 }
 
 func (cfg *apiConfig) handlerUsersCreate(w http.ResponseWriter, r *http.Request) {
@@ -74,28 +74,27 @@ func (cfg *apiConfig) handlerUserLogin(w http.ResponseWriter, r *http.Request) {
 	decoder := json.NewDecoder(r.Body)
 	params := parameters{}
 	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters", err)
+		return
+	}
 
-	log.Println(params.Email)
 	user, err := cfg.db.GetUser(r.Context(), params.Email)
 	if err != nil {
-		log.Println("Error email")
 		respondWithError(w, http.StatusUnauthorized, "Incorrect email or password", err)
 		return
 	}
 
 	err = auth.CheckPasswordHash(user.HashedPassword, params.Password)
 	if err != nil {
-		log.Println("Error passwd")
 		respondWithError(w, http.StatusUnauthorized, "Incorrect email or password", err)
 		return
 	}
 
 	respondWithJSON(w, http.StatusOK, response{
 		User: User{
-			ID:        user.ID,
-			CreatedAt: user.CreatedAt,
-			UpdatedAt: user.UpdatedAt,
-			Email:     user.Email,
+			ID:    user.ID,
+			Email: user.Email,
 		},
 	})
 }
--- chirpy/sql/schema/003_add_user_passwd.sql
+++
@@ -1,4 +0,0 @@
--- +goose Up
-ALTER TABLE users
-  ADD COLUMN hashed_password TEXT DEFAULT 'unset' NOT NULL;
---
