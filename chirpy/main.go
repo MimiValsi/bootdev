8db6542c7125489594c937e63e93c2d8e8656ffc
Miguel DA SILVA <miguel@dasilvaf.net>
2025-04-22 16:22:46 +0200

    add new handler to server mux

--- chirpy/main.go
+++ chirpy/main.go
@@ -62,6 +62,7 @@ func main() {
 
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
 	mux.HandleFunc("POST /api/users", apiCfg.handlerUsersCreate)
+	mux.HandleFunc("POST /api/login", apiCfg.handlerUserLogin)
 	mux.HandleFunc("GET /api/chirps", apiCfg.handlerFetchAllChirps)
 	mux.HandleFunc("GET /api/chirps/{ChirpID}", apiCfg.handlerFetchChirp)
 	mux.HandleFunc("POST /api/chirps", apiCfg.handlerChirps)
