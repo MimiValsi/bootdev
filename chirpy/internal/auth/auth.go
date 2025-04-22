96de6e3a068a504d0464a53bf71d1095e56c378d
Miguel Da Silva <miguel@dasilvaf.net>
2025-04-22 22:36:33 +0200

    create functions to create and validate JWT. Still need to test if it works has intended...

--- chirpy/internal/auth/auth.go
+++ chirpy/internal/auth/auth.go
@@ -1,6 +1,10 @@
 package auth
 
 import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -15,4 +19,39 @@ func HashPassword(password string) (string, error) {
 
 func CheckPasswordHash(hash, password string) error {
 	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+}
+
+func MakeJWT(UserID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	tn := time.Now().UTC()
+	exp := tn.Add(expiresIn)
+
+	claims := jwt.RegisteredClaims{
+		Issuer:    "chirpy",
+		IssuedAt:  jwt.NewNumericDate(tn),
+		ExpiresAt: jwt.NewNumericDate(exp),
+		Subject:   UserID.String(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	ss, err := token.SignedString([]byte(tokenSecret))
+	if err != nil {
+		return "", err
+	}
+
+	return ss, nil
+}
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &jwt.RegisteredClaims{}, func(token *jwt.Token) (any, error) {
+		return []byte(tokenSecret), nil
+	}, nil)
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	nt, err := token.Claims.GetSubject()
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	return uuid.MustParse(nt), nil
 }
