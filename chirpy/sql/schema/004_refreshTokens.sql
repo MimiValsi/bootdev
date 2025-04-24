faec2fad92585cfcdc0dee6aa0dcd940945b5dac
Miguel Da Silva <miguel@dasilvaf.net>
2025-04-24 20:42:21 +0200

    add NOT NULL to expires_at field on refresh_tokens table

--- chirpy/sql/schema/004_refreshTokens.sql
+++ chirpy/sql/schema/004_refreshTokens.sql
@@ -4,7 +4,7 @@ CREATE TABLE refresh_tokens (
   	created_at TIMESTAMP NOT NULL,
   	updated_at TIMESTAMP NOT NULL,
   	user_id UUID REFERENCES users(id) ON DELETE CASCADE NOT NULL,
-  	expires_at TIMESTAMP,
+  	expires_at TIMESTAMP NOT NULL,
   	revoked_at TIMESTAMP
 );
 --
