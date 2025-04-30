809e7e5f9d2ea3b24b3adce70c5a0a2fb29df0b8
Miguel DA SILVA <miguel@dasilvaf.net>
2025-04-30 18:43:02 +0200

    add new query to fetch all chirps from specific user

--- chirpy/sql/queries/chirps.sql
+++ chirpy/sql/queries/chirps.sql
@@ -22,6 +22,10 @@ SELECT * FROM chirps ORDER BY created_at ASC;
 SELECT * FROM chirps WHERE id = $1;
 --
 
+-- name: FetchChirpsByUserID :many
+SELECT * FROM chirps WHERE user_id = $1 ORDER BY created_at ASC;
+--
+
 -- name: DeleteChirpByID :exec
 DELETE FROM chirps WHERE id = $1 AND user_id = $2;
 --
