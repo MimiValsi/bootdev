0005fe491183b3529ad06e74f424490459f39355
Miguel DA SILVA <miguel@dasilvaf.net>
2025-04-30 13:30:30 +0200

    add query to delete user's chirp

--- chirpy/sql/queries/chirps.sql
+++ chirpy/sql/queries/chirps.sql
@@ -21,3 +21,7 @@ SELECT * FROM chirps ORDER BY created_at ASC;
 -- name: FetchSingleChirp :one
 SELECT * FROM chirps WHERE id = $1;
 --
+
+-- name: DeleteChirpByID :exec
+DELETE FROM chirps WHERE id = $1 AND user_id = $2;
+--
